2fe9a1d29fd1b642042834454ef8272e9f29ff96
Swat-UzB <61419443+Swat-UzB@users.noreply.github.com>
2021-08-19 11:29:09 +0500

    finish

--- app/src/main/java/com/example/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/example/dictionaryapp/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.dictionaryapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.dictionaryapp.databinding.ActivityMainBinding
 import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
+    private val KEY = "SHORT_DEF"
     private lateinit var binding: ActivityMainBinding
     private val DICTIONARY_WEBSITE =
         "https://www.dictionaryapi.com/api/v3/references/learners/json/"
@@ -20,22 +22,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val queue = Volley.newRequestQueue(this)
         binding.findButton.setOnClickListener {
-            val word = binding.searchEditText.text.toString()
-            val url = String.format("%s%s?key=%s", DICTIONARY_WEBSITE, word, API_KEY)
+
             val stringRequest = StringRequest(
-                Request.Method.GET, url,
-                Response.Listener<String> { response ->
-                    response
+                Request.Method.GET, getUrl(),
+                { response ->
+                    try {
+                        extractDefinitionFromJson(response)
+                    } catch (exception: Exception) {
+                        exception.printStackTrace()
+                    }
+
                 },
-                Response.ErrorListener { error ->
-                    error
+                { error ->
+                    Toast.makeText(this, error.message, Toast.LENGTH_SHORT)
                 }
             )
             queue.add(stringRequest)
         }
     }
 
+    private fun getUrl(): String {
+        val word = binding.searchEditText.text.toString()
+        return String.format("%s%s?key=%s", DICTIONARY_WEBSITE, word, API_KEY)
+    }
+
     private fun extractDefinitionFromJson(response: String) {
         val jsonArray = JSONArray(response)
+        val firstIndex = jsonArray.getJSONObject(0)
+        val getShortDefinition = firstIndex.getJSONArray("shortdef")
+        val getFirstShortDef = getShortDefinition.get(0).toString()
+
+        val intent = Intent(this, WordDefinitionActivity::class.java).apply {
+            putExtra(KEY, getFirstShortDef)
+        }
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/dictionaryapp/WordDefinitionActivity.kt
+++ app/src/main/java/com/example/dictionaryapp/WordDefinitionActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.dictionaryapp.databinding.ActivityWordDefinitionBinding
 
 class WordDefinitionActivity : AppCompatActivity() {
+    private val KEY = "SHORT_DEF"
     private lateinit var binding: ActivityWordDefinitionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWordDefinitionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.definitionTextView.text = intent.getStringExtra(KEY)
+        binding.backImageView.setOnClickListener { finish() }
     }
 }
